13d56c3b615b92fb7d3394d94c83486ee967a6f6
gerard <gerardmijares39@gmail.com>
2022-10-28 11:45:21 +0200

    program finished

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let question = "How are you?";
+    let person: String = "Bob".to_string();
+    let namaste = String::from("纳马斯特");
+
+    println!("{}! {} {}", namaste, question, person);
+}
